9904af676243b6c177ab3dcede62a593251df811
caimiao <caimiao@kingsoft.com>
2020-01-25 17:00:46 +0800

    为word2vec增加了词云显示

--- articlenlp/__init__.py
+++ articlenlp/__init__.py
@@ -28,10 +28,30 @@ def IndexAction():
 
 @nlp_app.route("/word2vec", methods=["GET", "POST"])
 def Word2vecIndexAction():
+    httpOperation = BaseOperations()
     if "GET" == request.method:
-        return render_template("nlp/_word2vec_index.html", **JointResponseParams(books= current_app.word2vecmodel.keys()))
+        posword, negword, book = httpOperation.getParams("posword", "negword", "book")
+        err_tips = None
+        relwords = None
+        if book in current_app.word2vecmodel:
+            query_pos = [p.strip() for p in posword.split(" ") if p.strip() != ""]
+            query_neg = [n.strip() for n in negword.split(" ") if n.strip() != ""]
+            query_pos = query_pos if len(query_pos) > 0 else None
+            query_neg = query_neg if len(query_neg) > 0 else None
+            try:
+                relwords = [m for m in current_app.word2vecmodel[book].most_similar(query_pos, query_neg, topn=100) if len(m[0]) > 1]
+            except KeyError as e:
+                err_tips = "查询词超出词汇表范围！"
+            except Exception as e:
+                err_tips = str(e)
+        return render_template("nlp/_word2vec_index.html", **JointResponseParams(books=current_app.word2vecmodel.keys(),
+                                                                                 relwords=relwords,
+                                                                                 curpos=posword,
+                                                                                 curneg=negword,
+                                                                                 curbook=book,
+                                                                                 err_tips=err_tips))
+        #return render_template("nlp/_word2vec_index.html", **JointResponseParams(books= current_app.word2vecmodel.keys()))
     elif "POST" == request.method:
-        httpOperation = BaseOperations()
         posword, negword, book = httpOperation.postParams("posword", "negword", "book")
         err_tips = None
         relwords = None
@@ -41,17 +61,16 @@ def Word2vecIndexAction():
             query_pos = query_pos if len(query_pos) > 0 else None
             query_neg = query_neg if len(query_neg) > 0 else None
             try:
-                relwords = current_app.word2vecmodel[book].most_similar(query_pos, query_neg, topn=100)
+                relwords = [m for m in current_app.word2vecmodel[book].most_similar(query_pos, query_neg, topn=100) if len(m[0]) > 1]
+                httpOperation.changeResponse2Success()
+                httpOperation.setData(relwords)
             except KeyError as e:
+                httpOperation.setFailureReason(str(e))
                 err_tips = "查询词超出词汇表范围！"
             except Exception as e:
                 err_tips = str(e)
-        return render_template("nlp/_word2vec_index.html", **JointResponseParams(books= current_app.word2vecmodel.keys(),
-                                                                                 relwords=relwords,
-                                                                                 curpos=posword,
-                                                                                 curneg=negword,
-                                                                                 curbook=book,
-                                                                                 err_tips=err_tips))
+                httpOperation.setFailureReason(err_tips)
+        return httpOperation.JsonResponse()
 
 @nlp_app.route("/kgindex", methods=["GET"])
 def KnowledgegraphIndexAction():
